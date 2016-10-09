f47edeb8d070920b3790a501d908d1dbe40f447c
Tino Schroeter <tino.schroeter@gmail.com>
2016-10-09 06:46:13 -1000

    first commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for i in $(ls template/);do
+output=$(echo "$i"|sed 's/.htsh//')
+if [ template/$i -nt www/$output.cgi ];then
+echo "generate www/$output.cgi file..."
+./bin/htsh template/$i www/$output.cgi
+chmod +x www/$output.cgi
+fi
+done
