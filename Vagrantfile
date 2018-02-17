f7e97fc983f526841630e9a0398e9a4890099262
Tino Schroeter <tino.schroeter@zeit.de>
2018-02-18 00:25:12 +0100

    update and clean up

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,30 @@
+$script = <<SCRIPT
+apt-get update; apt-get install -y apache2
+tee /etc/apache2/sites-enabled/000-default.conf >/dev/null <<EOF
+<VirtualHost *:80>
+	ServerName example.org
+	ServerAdmin webmaster@example.org
+	DocumentRoot /var/www/html/
+ 
+        ScriptAlias "/index.html" "/usr/lib/cgi-bin/doku.cgi"
+        ScriptAlias "/index" "/usr/lib/cgi-bin/doku.cgi"
+       
+        RedirectMatch 404 index.htsh
+
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+	CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+	Include conf-available/serve-cgi-bin.conf
+</VirtualHost>
+EOF
+service apache2 restart
+SCRIPT
+
 Vagrant.configure("2") do |config|
 config.vm.define "bos" do |bos|
   bos.vm.hostname = "bos"
   bos.vm.box = "precise64"
-  bos.vm.provision "shell", path: "install"
+  bos.vm.provision "shell", inline: $script
   bos.vm.network "forwarded_port", guest: 80, host: 9090
   bos.vm.synced_folder ".", "/home/vagrant"
   end
@@ -11,4 +33,3 @@ config.vm.provider "virtualbox" do |v|
   v.cpus = 2
   end
 end
-
--- build.sh
+++ build.sh
@@ -1,9 +1,95 @@
 #!/bin/bash
-for i in $(ls template/);do
-output=$(echo "$i"|sed 's/.htsh//')
-if [ template/$i -nt www/$output.cgi ];then
-echo "generate www/$output.cgi file..."
-./bin/htsh template/$i www/$output.cgi
-chmod +x www/$output.cgi
+#config
+
+cgi_path="/usr/lib/cgi-bin/"
+#######
+
+bos() {
+tee $2 >>/dev/null <<EOF
+#!/bin/bash 
+echo Content-type: text/html
+echo ""
+## make POST and GET stings 
+## as bash variables available
+if [ ! -z \$CONTENT_LENGTH ] && [ "\$CONTENT_LENGTH" -gt 0 ]; then
+read -n \$CONTENT_LENGTH POST_STRING <&0
+eval \`echo "\${POST_STRING}"|tr '&' ';'\`
+fi
+eval \`echo "\${QUERY_STRING}"|tr '&' ';'\`
+EOF
+
+flag=0
+lc=0
+txt=''
+while read -r line;do
+	let lc=lc+1
+	# Start and end tags in same line
+	echo "$line"| grep -qe "<?.*?>"  -qe "<?bash.*?>"
+	if [ $? -eq 0 ];then
+		if [ $flag -eq 0 ];then
+			line=${line//<?bash/<?}
+			pre=`echo -e "$line" |  sed -e 's/<?.*//'`
+			comm=`echo -e "$line" | sed -e 's/^.*<? //' | sed -e 's/?>.*//'`
+			post=`echo -e "$line" | sed -e 's/^.*?>//'`
+			
+			if [ -n "$pre" ];then
+				pre=${pre//\\/\\\\}
+				mod_pre=`echo -e "$pre" | sed "s/'/\\\'/g" | sed 's/"/\\\"/g'`
+				mod_pre=${mod_pre//\$/\\$}
+				echo -e "echo  \"$mod_pre\"" >>"$2"
+			fi
+			echo -e "$comm" >> "$2"
+			if [ -n "$post" ];then
+				post=${post//\\/\\\\}
+				mod_post=`echo -e "$post" | sed "s/'/\\\'/g" | sed 's/"/\\\"/g'`
+				mod_post=${mod_post//\$/\\$}
+				echo -e "echo  \"$mod_post\"" >>"$2"
+			fi
+			continue
+		else
+			echo -e "\n\n<br/><br/>  ERROR : Non-terminated &lt;?bash or &lt;? in file $1 (Line $lc) "
+			exit 1
+		fi	
+	fi
+	# Start tag
+	echo "$line" | sed 's/^ *//'| grep -q "^<?bash\|^<?"
+	if [ $? -eq 0 ];then
+		if [ $flag -eq 0 ];then
+			flag=1
+			line=${line/<?bash}
+			echo -e "${line/<?}" >> "$2"
+		else
+			echo -e "\n\n<br/><br/>  ERROR : Non-terminated &lt;?bash or &lt;? in file $1 (Line $lc) "
+			exit 1
+		fi
+		continue;
+	fi
+	# End tag
+	echo "$line"|grep -q "?>$"
+	if [ $? -eq 0 ];then
+		if [ $flag -eq 1 ];then
+			flag=0
+			echo -e"${line/?>}" >> "$2"
+		else
+			echo -e "\n\n<br/><br/>  ERROR : Unmatched ?&gt; in file $1 (Line $lc) "
+			exit 1
+		fi
+		continue;
+	fi
+	# Take action on current line based on flag
+	if [ $flag -eq 0 ];then
+		line=${line//\\/\\\\}
+		modline=`echo  "$line" |sed "s/'/\\\'/g" | sed 's/"/\\\"/g'`
+		modline=${modline//\$/\\$}
+		echo -e "echo  \"$modline\"" >>"$2"
+	else
+		echo -e "${line}" >> "$2"
+	fi
+done < "$1"
+}
+for i in $(ls |grep 'htsh$');do
+if [ $i -nt ${cgi_path}$(echo $i|sed 's/htsh/cgi/g') ];then
+echo "$i --->> ${cgi_path}$(echo $i|sed 's/htsh/cgi/g')"
+bos $i ${cgi_path}$(echo $i|sed 's/htsh/cgi/g')
 fi
 done
