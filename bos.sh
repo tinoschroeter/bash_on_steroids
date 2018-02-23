70d7b30ce69d6f7c2c7e1788b4e17cd1d2ec17f2
Tino Schroeter <tino.schroeter@zeit.de>
2018-02-24 00:33:06 +0100

    ShellCheck improved

--- bos.sh
+++ bos.sh
@@ -3,7 +3,7 @@
 cgi_path="/usr/lib/cgi-bin/"
 
 bos() {
-tee $2 >>/dev/null <<EOF
+tee "$2" >>/dev/null <<EOF
 #!/bin/bash 
 echo Content-type: text/html
 echo ""
@@ -18,58 +18,55 @@ EOF
 
 flag=0
 lc=0
-txt=''
+export txt=''
 while read -r line;do
-	let lc=lc+1
+	lc=$(( lc+1 ))
 	# Start and end tags in same line
-	echo "$line"| grep -qe "<?.*?>"  -qe "<?bash.*?>"
-	if [ $? -eq 0 ];then
+	if echo "$line"| grep -qe "<?.*?>"  -qe "<?bash.*?>";then
 		if [ $flag -eq 0 ];then
 			line=${line//<?bash/<?}
-			pre=`echo -e "$line" |  sed -e 's/<?.*//'`
-			comm=`echo -e "$line" | sed -e 's/^.*<? //' | sed -e 's/?>.*//'`
-			post=`echo -e "$line" | sed -e 's/^.*?>//'`
+			pre=$(echo -e "$line" |  sed -e 's/<?.*//')
+			comm=$(echo -e "$line" | sed -e 's/^.*<? //' | sed -e 's/?>.*//')
+			post=$(echo -e "$line" | sed -e 's/^.*?>//')
 			
 			if [ -n "$pre" ];then
 				pre=${pre//\\/\\\\}
-				mod_pre=`echo -e "$pre" | sed "s/'/\\\'/g" | sed 's/"/\\\"/g'`
+				mod_pre=$(echo -e "$pre" | sed "s/'/\\\'/g" | sed 's/"/\\\"/g')
 				mod_pre=${mod_pre//\$/\\$}
 				echo -e "echo  \"$mod_pre\"" >>"$2"
 			fi
 			echo -e "$comm" >> "$2"
 			if [ -n "$post" ];then
 				post=${post//\\/\\\\}
-				mod_post=`echo -e "$post" | sed "s/'/\\\'/g" | sed 's/"/\\\"/g'`
+				mod_post=$(echo -e "$post" | sed "s/'/\\\'/g" | sed 's/"/\\\"/g')
 				mod_post=${mod_post//\$/\\$}
 				echo -e "echo  \"$mod_post\"" >>"$2"
 			fi
 			continue
 		else
-			echo -e "\n\n<br/><br/>  ERROR : Non-terminated &lt;?bash or &lt;? in file $1 (Line $lc) "
+			echo  "ERROR : Non-terminated &lt;?bash or &lt;? in file $1 (Line $lc) "
 			exit 1
 		fi	
 	fi
 	# Start tag
-	echo "$line" | sed 's/^ *//'| grep -q "^<?bash\|^<?"
-	if [ $? -eq 0 ];then
+	if echo "$line" | sed 's/^ *//'| grep -q "^<?bash\|^<?";then
 		if [ $flag -eq 0 ];then
 			flag=1
 			line=${line/<?bash}
 			echo -e "${line/<?}" >> "$2"
 		else
-			echo -e "\n\n<br/><br/>  ERROR : Non-terminated &lt;?bash or &lt;? in file $1 (Line $lc) "
+			echo  "ERROR : Non-terminated &lt;?bash or &lt;? in file $1 (Line $lc) "
 			exit 1
 		fi
 		continue;
 	fi
 	# End tag
-	echo "$line"|grep -q "?>$"
-	if [ $? -eq 0 ];then
+	if echo "$line"|grep -q "?>$";then
 		if [ $flag -eq 1 ];then
 			flag=0
 			echo -e"${line/?>}" >> "$2"
 		else
-			echo -e "\n\n<br/><br/>  ERROR : Unmatched ?&gt; in file $1 (Line $lc) "
+			echo "ERROR : Unmatched ?&gt; in file $1 (Line $lc) "
 			exit 1
 		fi
 		continue;
@@ -77,7 +74,7 @@ while read -r line;do
 	# Take action on current line based on flag
 	if [ $flag -eq 0 ];then
 		line=${line//\\/\\\\}
-		modline=`echo  "$line" |sed "s/'/\\\'/g" | sed 's/"/\\\"/g'`
+		modline=$(echo  "$line" |sed "s/'/\\\'/g" | sed 's/"/\\\"/g')
 		modline=${modline//\$/\\$}
 		echo -e "echo  \"$modline\"" >>"$2"
 	else
