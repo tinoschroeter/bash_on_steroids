f5817ea0f06746d07f5f32800043aeb49356c0e4
Tino Schroeter <tino.schroeter@zeit.de>
2018-12-19 11:01:12 +0100

    improve bos script

--- bos.sh
+++ bos.sh
@@ -28,20 +28,20 @@ while read -r line;do
 	lc=$(( lc+1 ))
 	# Start and end tags in same line
 	if echo "$line"| grep -qe "<?.*?>"  -qe "<?bash.*?>";then
-		if [ $flag -eq 0 ];then
+		if [[ $flag -eq 0 ]];then
 			line=${line//<?bash/<?}
-			pre=$(echo -e "$line" |  sed -e 's/<?.*//')
+			pre=$(echo -e "$line"  |  sed -e 's/<?.*//')
 			comm=$(echo -e "$line" | sed -e 's/^.*<? //' | sed -e 's/?>.*//')
 			post=$(echo -e "$line" | sed -e 's/^.*?>//')
 			
-			if [ -n "$pre" ];then
+			if [[ -n "$pre" ]];then
 				pre=${pre//\\/\\\\}
 				mod_pre=$(echo -e "$pre" |sed 's/"/\\\"/g')
 				mod_pre=${mod_pre//\$/\\$}
 				echo -e "echo  \"$mod_pre\"" >>"$2"
 			fi
 			echo -e "$comm" >> "$2"
-			if [ -n "$post" ];then
+			if [[ -n "$post" ]];then
 				post=${post//\\/\\\\}
 				mod_post=$(echo -e "$post" | sed 's/"/\\\"/g')
 				mod_post=${mod_post//\$/\\$}
@@ -55,7 +55,7 @@ while read -r line;do
 	fi
 	# Start tag
 	if echo "$line" | sed 's/^ *//'| grep -q "^<?bash\\|^<?";then
-		if [ $flag -eq 0 ];then
+		if [[ $flag -eq 0 ]];then
 			flag=1
 			line=${line/<?bash}
 			echo -e "${line/<?}" >> "$2"
@@ -67,17 +67,17 @@ while read -r line;do
 	fi
 	# End tag
 	if echo "$line"|grep -q "?>$";then
-		if [ $flag -eq 1 ];then
+		if [[ $flag -eq 1 ]];then
 			flag=0
 			echo "${line/?>}" >> "$2"
 		else
-			echo "ERROR : Unmatched ?&gt; in file $1 (Line $lc) "
+			echo "ERROR : Unmatched ?&gt; in file $1 (Line $lc)"
 			exit 1
 		fi
-		continue;
+	 continue;
 	fi
    # Take action on current line based on flag
-    if [ $flag -eq 0 ];then
+    if [[ $flag -eq 0 ]];then
         line=${line//\\/\\\\}
         modline=$(echo  -e "$line" | sed -e 's/"/\\\"/g')
         modline=${modline//\$/\\$}
@@ -88,9 +88,9 @@ while read -r line;do
 done < "$1"
 }
 for file in *.htsh;do
-  if [ "$file" -nt ${cgi_path}"${file//htsh/cgi}" ];then
-    echo -e "\033[0;32m$file \033[0;31m--->>\033[0;32m ${cgi_path}${file//htsh/cgi}\033[0m"
-    bos "$file" ${cgi_path}"${file//htsh/cgi}"
-    chmod +x ${cgi_path}"${file//htsh/cgi}"
+  if [[ "$file" -nt ${cgi_path}"${file//htsh/cgi}" ]];then
+     echo -e "\033[0;32m$file \033[0;31m--->>\033[0;32m ${cgi_path}${file//htsh/cgi}\033[0m"
+     bos "$file" ${cgi_path}"${file//htsh/cgi}"
+     chmod +x ${cgi_path}"${file//htsh/cgi}"
   fi
 done
