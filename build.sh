ab811e2be79180e4caa50a51409e9751555fa339
Tino Schroeter <tino.schroeter@zeit.de>
2019-05-13 23:02:20 +0200

    check QUERY_STRING

--- build.sh
+++ build.sh
@@ -11,14 +11,11 @@ echo Content-type: text/html
 echo ""
 ## make POST and GET stings 
 ## as bash variables available
-
-if [[ \$QUERY_STRING =~ [a-z-]*=[a-z]*$ ]] || [[ \$POST_STRING =~ [a-z-]*=[a-z]*$ ]];then
-  if [ ! -z \$CONTENT_LENGTH ] && [ "\$CONTENT_LENGTH" -gt 0 ] && [ \$CONTENT_TYPE != "multipart/form-data" ]; then
-  read -n \$CONTENT_LENGTH POST_STRING <&0
-  eval \`echo "\${POST_STRING//;}"|tr '&' ';'\`
-  fi
-  eval \`echo "\${QUERY_STRING//;}"|tr '&' ';'\`
+if [ ! -z \$CONTENT_LENGTH ] && [ "\$CONTENT_LENGTH" -gt 0 ] && [ \$CONTENT_TYPE != "multipart/form-data" ]; then
+read -n \$CONTENT_LENGTH POST_STRING <&0
+eval \`echo "\${POST_STRING//;}"|tr '&' ';'|grep '[a-z]*=[a-z]*$'\`
 fi
+eval \`echo "\${QUERY_STRING//;}"|tr '&' ';'|grep '[a-z]*=[a-z]*$'\`
 ## decode URL-encoding
 urldecode() { : "\${*//+/ }"; echo -e "\${_//%/\\x}"; }
 EOF
